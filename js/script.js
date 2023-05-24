1b5d50c34180ce216f7224a84e0c92cd0d064265
Marcella Vizoná Vasconcelos <126676999+MarcellaVizona@users.noreply.github.com>
2023-05-24 16:58:39 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-alert("Olá mundo")
+/* Variaveis*/
+let btn1 = 0;
+let btn2 = 0;
+let btn3 = 0;
+let btn4 = 0;
+let btn5 = 0;
+let btn6 = 0;
+let btn7 = 0;
+let btn8 = 0;
+let btn9 = 0;
+let controle = 0;
+let jogada = 0;
+let FinalJogo = false;
+
+/* Click botão 1 */
+ function clickBtn1(){
+    let btn = document.getElementById('btn1');
+     
+    if ( btn.value == " " && !FinalJogo){
+        if (controle == 0){
+            controle == 1;
+            btn.value = "X";
+            btn1 ="X";
+        } else {
+            controle = 0;
+            btn.value = "0";
+            btn1 ="O";
+
+        }
+
+
+    }
+ }
