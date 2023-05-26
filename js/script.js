0dec6c8d3d9360314ab84d6f9e73f104fcf886c9
Marcella Vizoná Vasconcelos <126676999+MarcellaVizona@users.noreply.github.com>
2023-05-26 09:37:26 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -18,13 +18,172 @@ let FinalJogo = false;
      
     if ( btn.value == " " && !FinalJogo){
         if (controle == 0){
-            controle == 1;
+            controle = 1;
             btn.value = "X";
-            btn1 ="X";
+            btn1 = "X";
         } else {
             controle = 0;
-            btn.value = "0";
+            btn.value = "O";
             btn1 ="O";
+
+        }
+
+
+    }
+ }
+
+ /* Click botão 2 */
+ function clickBtn2(){
+    let btn = document.getElementById('btn2');
+     
+    if ( btn.value == " " && !FinalJogo){
+        if (controle == 0){
+            controle = 1;
+            btn.value = "X";
+            btn2 ="X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn2 ="O";
+
+        }
+
+
+    }
+ }
+
+ /* Click botão 3 */
+ function clickBtn3(){
+    let btn = document.getElementById('btn3');
+     
+    if ( btn.value == " " && !FinalJogo){
+        if (controle == 0){
+            controle = 1;
+            btn.value = "X";
+            btn3 ="X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn3 ="O";
+
+        }
+
+
+    }
+ }
+
+ /* Click botão 4*/
+ function clickBtn4(){
+    let btn = document.getElementById('btn4');
+     
+    if ( btn.value == " " && !FinalJogo){
+        if (controle == 0){
+            controle = 1;
+            btn.value = "X";
+            btn4 ="X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn4 ="O";
+
+        }
+
+
+    }
+ }
+
+ /* Click botão 5 */
+ function clickBtn5(){
+    let btn = document.getElementById('btn5');
+     
+    if ( btn.value == " " && !FinalJogo){
+        if (controle == 0){
+            controle = 1;
+            btn.value = "X";
+            btn5 ="X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn5 ="O";
+
+        }
+
+
+    }
+ }
+
+ /* Click botão 6 */
+ function clickBtn6(){
+    let btn = document.getElementById('btn6');
+     
+    if ( btn.value == " " && !FinalJogo){
+        if (controle == 0){
+            controle = 1;
+            btn.value = "X";
+            btn6 ="X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn6 ="O";
+
+        }
+
+
+    }
+ }
+
+ /* Click botão 7 */
+ function clickBtn7(){
+    let btn = document.getElementById('btn7');
+     
+    if ( btn.value == " " && !FinalJogo){
+        if (controle == 0){
+            controle = 1;
+            btn.value = "X";
+            btn7 ="X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn7 ="O";
+
+        }
+
+
+    }
+ }
+
+ /* Click botão 8 */
+ function clickBtn8(){
+    let btn = document.getElementById('btn8');
+     
+    if ( btn.value == " " && !FinalJogo){
+        if (controle == 0){
+            controle = 1;
+            btn.value = "X";
+            btn8 ="X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn8 ="O";
+
+        }
+
+
+    }
+ }
+ /* Click botão 9 */
+ function clickBtn9(){
+    let btn = document.getElementById('btn9');
+     
+    if ( btn.value == " " && !FinalJogo){
+        if (controle == 0){
+            controle = 1;
+            btn.value = "X";
+            btn9 ="X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn9 ="O";
 
         }
 
